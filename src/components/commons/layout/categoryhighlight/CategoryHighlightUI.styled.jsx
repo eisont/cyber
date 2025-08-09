6587881864c58c97ce13a5a3b0d35597021524e3
치훈 <rlaclgns321@naver.com>
2025-08-10 00:06:58 +0900

    feat(ui): 카테고리 하이라이트 컴포넌트 구조와 스타일 추가

---
+++ src/components/commons/layout/categoryhighlight/CategoryHighlightUI.styled.jsx
@@ -0,0 +1,42 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+  height: 352px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TotalBox = styled.div`
+  width: 1440px;
+  height: 352px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background: #fafafa;
+`;
+
+export const MainBox = styled.div`
+  width: 1120px;
+  height: 190px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+export const Top = styled.div`
+  cursor: default;
+  font-weight: 500;
+  font-size: 24px;
+`;
+export const Categories = styled.div`
+  width: 1120px;
+  height: 128px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
