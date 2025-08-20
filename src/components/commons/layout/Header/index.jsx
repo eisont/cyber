536ae84d75e3545f98b6f1a05f7e0641ed33572b
치훈 <rlaclgns321@naver.com>
2025-08-20 22:25:27 +0900

    refactor: Header 불필요한 코드 제거

--- src/components/commons/layout/Header/index.jsx
+++ src/components/commons/layout/Header/index.jsx
@@ -1,34 +1,19 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import * as S from './Header.styled';
 
 const Header = () => {
-  const location = useLocation();
-  const pathname = location.pathname;
-
   return (
     <S.Wrapper>
       <S.TotalBox>
-        <Link to='/'>
-          <S.Logo src='../../../../../public/logo/logo.png' />
-        </Link>
-        <S.InputBox>
-          <S.SearchIcon src='../../../../../public/icon/Search.png' />
-          <S.Input type='text' placeholder='Search'></S.Input>
-        </S.InputBox>
-        <S.MenuBox>
-          <S.LinkSt to='/' pathname={pathname === '/'}>
-            Home
-          </S.LinkSt>
-          <S.LinkSt to='/about' pathname={pathname === '/about'}>
-            About
-          </S.LinkSt>
-          <S.LinkSt to='/contact' pathname={pathname === '/contact'}>
-            Contact Us
-          </S.LinkSt>
-          <S.LinkSt to='/blog' pathname={pathname === '/blog'}>
-            Blog
-          </S.LinkSt>
-        </S.MenuBox>
+        <S.FlexBox>
+          <Link to='/'>
+            <S.Logo src='../../../../../public/logo/logo.png' />
+          </Link>
+          <S.InputBox>
+            <S.SearchIcon src='../../../../../public/icon/Search.png' />
+            <S.Input type='text' placeholder='Search'></S.Input>
+          </S.InputBox>
+        </S.FlexBox>
         <S.IconBox>
           <S.LinkSt to='/favorites'>
             <S.Icon src='../../../../../public/icon/Favorites.png' />
