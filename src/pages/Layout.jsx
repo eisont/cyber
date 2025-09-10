934027be67ecad14e7cb27bd471053361407ef92
치훈 <rlaclgns321@naver.com>
2025-09-10 23:39:19 +0900

    feat: CategoryHighlight 모바일 반응형 적용

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -24,7 +24,7 @@ const Layout = () => {
           element={
             <>
               {/* <HeroBanner /> */}
-              {/* <CategoryHighlight /> */}
+              <CategoryHighlight />
               <ProductGrid />
               <CategoryPromoBanner />
               <SeasonalSaleBanner />
