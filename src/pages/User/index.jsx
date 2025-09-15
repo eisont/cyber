25ba1122616773ad6588bca0c5ee262114ba06ab
치훈 <rlaclgns321@naver.com>
2025-09-15 14:52:39 +0900

    feat: 마이페이지 생성

---
+++ src/pages/User/index.jsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+import CartPage from './ui/Cart';
+
+const Wrapper = styled.div``;
+
+const UserPage = () => {
+  return (
+    <Wrapper>
+      유저페이지
+      <CartPage />
+    </Wrapper>
+  );
+};
+
+export default UserPage;
