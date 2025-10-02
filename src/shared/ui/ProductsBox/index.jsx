567e7e0f3fc82c43cef22b07c510f73b6a62782b
치훈 <rlaclgns321@naver.com>
2025-10-02 18:59:05 +0900

    refactor: itemData → ItemData 네이밍 일관성 적용

--- src/shared/ui/ProductsBox/index.jsx
+++ src/shared/ui/ProductsBox/index.jsx
@@ -14,17 +14,13 @@ const ProductsBox = () => {
         <S.ProductsCount>
           Selected Products: <S.Count>{ProductListData ? ProductListData.length : 0}</S.Count>
         </S.ProductsCount>
-
-        {/* <S.SeleteBox name='Dropdown'>
-          <option value=''>By rating</option>
-        </S.SeleteBox> */}
       </S.FlexBox>
 
       <S.ProductsBox>
         {ProductListData && (
           <>
             {ProductListData.map((el) => (
-              <ProductItem key={el.id} itemData={el} />
+              <ProductItem key={el.id} ItemData={el} />
             ))}
           </>
         )}
