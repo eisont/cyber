b496b2b160f37e77f5f447c108dc1bcdb913c371
치훈 <rlaclgns321@naver.com>
2025-10-09 16:27:32 +0900

    feat: ProductGrid 컴포넌트에 스켈레톤 UI 적용

--- src/shared/ui/ProductGrid/index.jsx
+++ src/shared/ui/ProductGrid/index.jsx
@@ -5,11 +5,12 @@ import * as S from '@/shared/ui/ProductGrid/ProductGrid.styled';
 
 import { useFetch } from '@/shared/hooks/useFetchHooks';
 import { ToUpper } from '@/shared/lib';
+import { DumImg } from '@/shared/assets/styled/skelepton';
 
 const ProductGrid = () => {
   const productId = useSelector((state) => state.productId);
 
-  const [CategoryListData] = useFetch({ resource: 'products', endPoint: 'category-list', suffix: '', enabled: true });
+  const [CategoryListData, isLoading] = useFetch({ resource: 'products', endPoint: 'category-list', suffix: '', enabled: true });
   const [data, ProductListisLoading] = useFetch({ resource: 'products', endPoint: 'category', suffix: '/', params: productId, enabled: true });
   const ProductListData = data.products;
 
@@ -18,21 +19,25 @@ const ProductGrid = () => {
   return (
     <S.Wrapper>
       <S.TotalBox>
-        <S.MainBox>
-          <S.CategoryBox>
-            {CategoryListData?.map((el) => (
-              <S.Category key={Number(new Date()) + el} id={el} productId={productId} onClick={() => dispatch(productIdSlice.actions.getProductId(el))}>
-                {ToUpper(el)}
-              </S.Category>
-            ))}
-          </S.CategoryBox>
+        {isLoading ? (
+          <DumImg width='1200px' height='1060px' />
+        ) : (
+          <S.MainBox>
+            <S.CategoryBox>
+              {CategoryListData?.map((el) => (
+                <S.Category key={Number(new Date()) + el} id={el} productId={productId} onClick={() => dispatch(productIdSlice.actions.getProductId(el))}>
+                  {ToUpper(el)}
+                </S.Category>
+              ))}
+            </S.CategoryBox>
 
-          <S.ProductsItemsBox>
-            {ProductListData?.map((el) => (
-              <ProductItem key={el.id} ItemData={el} isLoading={ProductListisLoading} />
-            ))}
-          </S.ProductsItemsBox>
-        </S.MainBox>
+            <S.ProductsItemsBox>
+              {ProductListData?.map((el) => (
+                <ProductItem key={el.id} ItemData={el} isLoading={ProductListisLoading} />
+              ))}
+            </S.ProductsItemsBox>
+          </S.MainBox>
+        )}
       </S.TotalBox>
     </S.Wrapper>
   );
