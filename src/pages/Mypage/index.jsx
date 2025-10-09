b0115c7362aa4cee968f4d280d80f2eb0b462548
치훈 <rlaclgns321@naver.com>
2025-10-09 16:43:28 +0900

    perf: mypage 컴포넌트에 lazy 및 Suspense 적용

--- src/pages/Mypage/index.jsx
+++ src/pages/Mypage/index.jsx
@@ -1,15 +1,9 @@
 import * as S from './MyPage.styled';
-import ProfileCard from '@/pages/Mypage/ui/ProfileCard';
-import PersonalCard from '@/pages/Mypage/ui/PersonalCard';
-import AddressCard from '@/pages/Mypage/ui/AddressCard';
-import WorkEduCard from '@/pages/Mypage/ui/WorkEduCard';
-import FinanceCard from '@/pages/Mypage/ui/FinanceCard';
-import SystemCard from '@/pages/Mypage/ui/SystemCard';
 import { useSelector } from 'react-redux';
-import CartCard from '@/pages/Mypage/ui/Cart';
 import { useState } from 'react';
+import CartCard from '@/pages/Mypage/ui/Cart';
+import { ProfileCard, PersonalCard, AddressCard, WorkEduCard, FinanceCard, SystemCard } from '@/pages/Mypage/ui';
 
-// props로 user 주입 or 전역(store)에서 select
 const MyPage = () => {
   const userInfo = useSelector((state) => state.userInfo);
   const [toggle, setToggle] = useState(false);
