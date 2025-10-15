d305eb85215595f8f4f7bf55a51b15471f534ca1
치훈 <rlaclgns321@naver.com>
2025-10-15 15:36:38 +0900

    refactor: App 및 router 파일 분리로 코드 구조 세분화

---
+++ src/app/router/index.jsx
@@ -0,0 +1,43 @@
+import { useSelector } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+
+import Layout from '@/pages/Layout';
+const Category = lazy(() => import('@/pages/Category'));
+const SearchProducts = lazy(() => import('@/pages/SearchProducts'));
+const SelectUser = lazy(() => import('@/pages/SelectUser'));
+const MyPage = lazy(() => import('@/pages/MyPage'));
+import ProductGrid from '@/shared/ui/ProductGrid';
+import { HeroBanner, CategoryHighlight, CategoryPromoBanner, SeasonalSaleBanner } from '@/pages/ui';
+
+const AppRouter = () => {
+  const userInfo = useSelector((state) => state.userInfo);
+
+  return (
+    <Layout>
+      <Suspense fallback={<>loading...</>}>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <>
+                <HeroBanner />
+                <CategoryHighlight />
+                <ProductGrid />
+                <CategoryPromoBanner />
+                <SeasonalSaleBanner />
+              </>
+            }
+          />
+
+          <Route path='/category/:id?' element={<Category />} />
+          <Route path='/products' element={<SearchProducts />} />
+          <Route path='/selectuser' element={<SelectUser />} />
+          <Route path={`/${userInfo.username}`} element={<MyPage />} />
+        </Routes>
+      </Suspense>
+    </Layout>
+  );
+};
+
+export default AppRouter;
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,18 +1,7 @@
-// 무한스크롤, Ref 구현, 찜한 목록,
-// 모바일 반응형 구현 (상품 디테일, 카테고리>필터, header_모달 )
-
-import { lazy, Suspense } from 'react';
-import { useSelector } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
-import styled from '@emotion/styled';
 import { BREAKPOINTS } from '@/shared/assets/styled/breakpoints';
 import { Header, Footer } from '@/shared/layout';
-const Category = lazy(() => import('@/pages/Category'));
-const SearchProducts = lazy(() => import('@/pages/SearchProducts'));
-const SelectUser = lazy(() => import('@/pages/SelectUser'));
-const MyPage = lazy(() => import('@/pages/MyPage'));
-import ProductGrid from '@/shared/ui/ProductGrid';
-import { HeroBanner, CategoryHighlight, CategoryPromoBanner, SeasonalSaleBanner } from '@/pages/ui';
+
+import styled from '@emotion/styled';
 
 const Wrapper = styled.div`
   display: flex;
@@ -26,33 +15,11 @@ const Wrapper = styled.div`
   }
 `;
 
-const Layout = () => {
-  const userInfo = useSelector((state) => state.userInfo);
-
+const Layout = ({ children }) => {
   return (
     <Wrapper>
       <Header />
-      <Suspense fallback={<>loading...</>}>
-        <Routes>
-          <Route
-            path='/'
-            element={
-              <>
-                <HeroBanner />
-                <CategoryHighlight />
-                <ProductGrid />
-                <CategoryPromoBanner />
-                <SeasonalSaleBanner />
-              </>
-            }
-          />
-
-          <Route path='/category/:id?' element={<Category />} />
-          <Route path='/products' element={<SearchProducts />} />
-          <Route path='/selectuser' element={<SelectUser />} />
-          <Route path={`/${userInfo.username}`} element={<MyPage />} />
-        </Routes>
-      </Suspense>
+      {children}
       <Footer />
     </Wrapper>
   );
